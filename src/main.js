36a756cc12fead54cc68759b48e37b0c52152f25
Trésor ILOYI <tresor.iloyi@gmail.com>
2021-07-30 13:47:14 +0200

    feat: implement a local import with Vue.use

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import MozaicVue from "@mozaic-ds/vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// import "@mozaic-ds/vue/dist/mozaic-vue.css"; // Import the css of all components (158ko)
+
+Vue.use(MozaicVue);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
