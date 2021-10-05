b20b5806b76412b3c8da2309e2c34537f2344c83
Trésor ILOYI <tresor.iloyi@gmail.com>
2021-10-05 10:03:22 +0200

    feat: implement global import

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <m-accordion id="default" label="Accordion">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum rerum
+          alias corporis, odit, dolor non doloribus tenetur ullam distinctio
+          ducimus ipsum corrupti, magni dolore. Alias voluptatibus excepturi
+          assumenda nisi fugit.
+        </p>
+      </m-accordion>
+      <m-button label="Ceci est un bouton"></m-button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style lang="scss">
@@ -24,5 +35,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.container {
+  margin: 0 auto;
+  max-width: 980px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+import MozaicVue from "@mozaic-ds/vue";
+// import { MAccordion } from "@mozaic-ds/vue";
+
+import "@mozaic-ds/vue/dist/mozaic-vue.css";
+
+Vue.config.productionTip = false;
+
+Vue.use(MozaicVue);
+// Vue.use(MAccordion);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
