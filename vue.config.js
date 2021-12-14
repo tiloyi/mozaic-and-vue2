19806ad357f52810309008dfd4298f94a8a805e5
Trésor ILOYI <tresor.iloyi@gmail.com>
2021-12-14 16:09:42 +0100

    feat: implement vue.config.js to manage local import

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+let pluginList = require("@mozaic-ds/css-dev-tools/postcssPluginConfig");
+const scssSyntax = require("postcss-scss");
+
+// Remove stylelint plugin to use it as a webpack plugin due to an incompatibility
+// between postcss stylelint plugin and vue-loader.
+// See https://github.com/vuejs/vue-loader/issues/627#issuecomment-277753794
+pluginList = pluginList.filter(
+  ({ postcssPlugin }) => postcssPlugin !== "stylelint"
+);
+
+module.exports = {
+  productionSourceMap: process.env.MOZAIC_PRESET === "adeo" ? false : true,
+  chainWebpack: (config) => {
+    config.module
+      .rule("postcss-scss")
+      .test(/\.scss$/)
+      .use("vue-style-loader")
+      .loader("postcss-loader")
+      .tap((options) => {
+        options = {
+          postcssOptions: {
+            syntax: scssSyntax,
+            plugins: pluginList,
+          },
+        };
+        return options;
+      });
+  },
+};
